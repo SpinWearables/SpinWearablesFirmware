c853ede6b2ece97ec4e88319310e7d51500e6af9
Stefan Krastanov <stefan@krastanov.org>
2020-02-10 22:03:40 -0500

    Expand Biology of sight lesson.

---
+++ examples/BiologyOfSight_2_persistence_of_vision/BiologyOfSight_2_persistence_of_vision.ino
@@ -0,0 +1,23 @@
+#include "SpinWearables.h"
+using namespace SpinWearables;
+
+void setup() {
+  // Initialize all of the hardware on the SpinWheel.
+  SpinWheel.begin();
+}
+
+// CHANGE THIS VALUE! EXPERIMENT!
+int delay_time_in_milliseconds = 500;
+
+void loop() {
+  // Show red...
+  SpinWheel.setLargeLEDsUniform(255, 0, 0);
+  SpinWheel.drawFrame();
+  // Wait...
+  delay(delay_time_in_milliseconds);
+  // Show blue...
+  SpinWheel.setLargeLEDsUniform(0, 0, 255);
+  SpinWheel.drawFrame();
+  // Wait...
+  delay(delay_time_in_milliseconds);  
+}
