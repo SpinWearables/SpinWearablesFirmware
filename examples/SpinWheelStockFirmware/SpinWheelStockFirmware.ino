c69ce8a7288e5fe6bb8cba37817a257008e7d441
Bridget Hegarty <beh55@cornell.edu>
2020-05-02 21:04:13 -0400

    Changed pre-loaded routines.

--- examples/SpinWheelStockFirmware/SpinWheelStockFirmware.ino
+++ examples/SpinWheelStockFirmware/SpinWheelStockFirmware.ino
@@ -8,24 +8,18 @@
 using namespace SpinWearables;
 
 void setup() {
-  Serial.begin(115200);
   SpinWheel.begin();
-  SpinWheel.addAnimationRoutine(tiltSensor2);
-  SpinWheel.addAnimationRoutine(tiltSensor3);
+  SpinWheel.largeLEDs.setBrightness(20);
+  bootAnimation();
   SpinWheel.addAnimationRoutine(largeRainbow);
   SpinWheel.addAnimationRoutine(smallWhiteRotating);
   SpinWheel.addAnimationRoutine(allBreathing);
-  SpinWheel.addAnimationRoutine(tiltSensor);
-  SpinWheel.addAnimationRoutine(compass);
-  //SpinWheel.addAnimationRoutine(flashlight);
-  bootAnimation();
-  SpinWheel.largeLEDs.setBrightness(20);
+  SpinWheel.addAnimationRoutine(tiltSensor3);
+  SpinWheel.addAnimationRoutine(tealLight);
 }
 
 void loop() {
   SpinWheel.readIMU();
   SpinWheel.runAnimationRoutine();
   SpinWheel.drawFrame();
-  Serial.print(executionTime()); Serial.print('\t'); Serial.print(sqrt(SpinWheel.ax*SpinWheel.ax+SpinWheel.ay*SpinWheel.ay+SpinWheel.az*SpinWheel.az));
-  Serial.println();
 }
