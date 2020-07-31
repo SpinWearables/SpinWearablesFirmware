32e072dc08a24ff98be4d417f123864a408fc7e7
Bridget Hegarty <beh55@cornell.edu>
2020-07-31 00:41:54 -0400

    Updated the Grand_Finale Dancing sketch.

--- examples/Dancing_with_Color/Grand_Finale/Grand_Finale.ino
+++ examples/Dancing_with_Color/Grand_Finale/Grand_Finale.ino
@@ -26,52 +26,56 @@ int offset = 0;
 int colorChange;
 uint8_t angle = 0; 
 
+int a_cum = 0;
+
 // Instructions in a loop function are repeated over and over again,
 // in other words, "in a loop".
 void loop() {
 // The `readIMU` function checks if the sensor is ready
 // and takes its current rotation data.
   SpinWheel.readIMU();
-// Here, we use an **if statement** to check if 
-// the rotation is fast. If this is true, then add 
-// a step to the offset.
-  if (abs(SpinWheel.gx) > 1) {
-    offset = SpinWheel.gx*100; 
-    Serial.println(offset);
+
+// First, we can add a "motion_snake" like in the previous sketch.
+  if (abs(SpinWheel.gx) >= 1) {
+     // add 10 to make it spin at a reasonable speed
+     angle = angle+SpinWheel.gx+20;
+     SpinWheel.setSmallLEDsPointer(angle, 255, 255, 255);
   }
 
-// Here we will use a **for loop**. Similar to the 
-// `loop` function, instructions inside the for loop
-// repeat. In this case the insturctions inside the 
-// for loop will repeat four times as specified by the 
-// `i < 4`. This will make a rainbow in the large LEDs
-// with a color change specified by the movement of the 
-// device. 
-  for (int i=0; i<4; i++) {
-    colorChange = offset+i*255/4;
-    Serial.println(colorChange);
-    SpinWheel.setLargeLED(i, colorWheel(colorChange));
-    SpinWheel.setLargeLED(7-i, colorWheel(colorChange));
-  }
+// Rather than having the color of the large LEDs change
+// based on the SpinWheel's angular velocity, instead
+// we can have the color change when the acceleration
+// of the SpinWheel is sufficient.  
+  int a = abs(SpinWheel.ax);
+// Like r_cum in the Rainbow_Chase_Advanced sketch, a_cum
+// is the sum of the SpinWheel's acceleration in the x 
+// direction. We multiply the acceleration by 10 to have
+// the color change be noticeable. We did a similar thing
+// in the snake example.
+  a_cum = (a)*10 + a_cum;
+// We will have some fixed "delay" between the
+// numbers controlling each LED's color.
+// Change this number! What happens?
+  int a_delay = 15;
+// We will have the inner and outer large LEDs
+// light up the same color. The color of each pair
+// will be governed by a slightly modified number.
+  int a0 = a_cum % 255;
+  int a1 = (a_cum+a_delay) % 255;
+  int a2 = (a_cum+2*a_delay) % 255;
+  int a3 = (a_cum+3*a_delay) % 255;
+// Here we finally set those colors. 
+  SpinWheel.setLargeLED(0, colorWheel(a0));
+  SpinWheel.setLargeLED(1, colorWheel(a1));
+  SpinWheel.setLargeLED(2, colorWheel(a2));
+  SpinWheel.setLargeLED(3, colorWheel(a3));
+  SpinWheel.setLargeLED(4, colorWheel(a0));
+  SpinWheel.setLargeLED(5, colorWheel(a1));
+  SpinWheel.setLargeLED(6, colorWheel(a2));
+  SpinWheel.setLargeLED(7, colorWheel(a3));
 
-// Here we define the total acceleration as the 
-// sum of the acceleration in the x,y,and z directions.
-  float total_acceleration = SpinWheel.ax + SpinWheel.ay + SpinWheel.az;
+
   
-// Here we use an **if statement** to check to see if the 
-// the total acceleration is large enough. If it is, 
-// then we will create a snake on the small LEDs using the function
-// `SpinWheel.setSmallLEDsPointer()`. Here we use (0,255,0) 
-// to make the snake green, but you can use any color you like!
-// We have also included the 5 here to make the SpinWheel spin at 
-// a reasonable speed, but you can also adjust this to make the 
-// SpinWheel spin faster or slower.  
-  if (abs(total_acceleration) > 1) { 
-     angle = angle+SpinWheel.gz+5;  
-     SpinWheel.setSmallLEDsPointer(angle, 0, 255, 0);
-  }
-// Additionally, light up the large LEDs. 
-  SpinWheel.setLargeLEDsUniform(100, 0, 100);
 // Have the SpinWheel display the desired pattern.
   SpinWheel.drawFrame();
 }
