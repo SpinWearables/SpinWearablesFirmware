e9075cf21605eb7fa8bffeb73d7b19af7b532957
Hannah Bossi <hannahbossi@Hannahs-MacBook-Pro-3.local>
2020-07-30 20:42:29 -0400

    [examples/Dancing_with_Color/]: Add the grand finale example with updated snake code.

--- examples/Dancing_with_Color/Grand_Finale/Grand_Finale.ino
+++ examples/Dancing_with_Color/Grand_Finale/Grand_Finale.ino
@@ -20,9 +20,11 @@ void setup() {
   SpinWheel.begin();
 }
 
-
+// Here, we initialize some variables that we
+// will make use of later. 
 int offset = 0;
 int colorChange;
+uint8_t angle = 0; 
 
 // Instructions in a loop function are repeated over and over again,
 // in other words, "in a loop".
@@ -58,14 +60,18 @@ void loop() {
   
 // Here we use an **if statement** to check to see if the 
 // the total acceleration is large enough. If it is, 
-// then we will create a snake on the small LEDs using
-// `SpinWheel.snake()`. Here we use (0,255,0) to make the 
-// snake green, but you can use any color you like! 
+// then we will create a snake on the small LEDs using the function
+// `SpinWheel.setSmallLEDsPointer()`. Here we use (0,255,0) 
+// to make the snake green, but you can use any color you like!
+// We have also included the 5 here to make the SpinWheel spin at 
+// a reasonable speed, but you can also adjust this to make the 
+// SpinWheel spin faster or slower.  
   if (abs(total_acceleration) > 1) { 
-     SpinWheel.snake(0,255,0);
+     angle = angle+SpinWheel.gz+5;  
+     SpinWheel.setSmallLEDsPointer(angle, 0, 255, 0);
   }
-
-
-
+// Additionally, light up the large LEDs. 
+  SpinWheel.setLargeLEDsUniform(100, 0, 100);
+// Have the SpinWheel display the desired pattern.
   SpinWheel.drawFrame();
 }
