d550de2d0465083632540bbc3448851fcc02a740
Bridget Hegarty <beh55@cornell.edu>
2020-08-28 20:40:22 -0400

    Fixed Update_Which_LED sketch.

--- examples/Paper_Guide/Update_Which_LED/Update_Which_LED.ino
+++ examples/Paper_Guide/Update_Which_LED/Update_Which_LED.ino
@@ -1,12 +1,8 @@
 #include "SpinWearables.h"
 using namespace SpinWearables;
+
 void setup() {
   SpinWheel.begin();
-  // This line ensures that the communication hardware
-  // on our device is ready to send messages.
-  // The name "Serial" is such for historical reasons
-  // (it is the name for this type of communication).
-  Serial.begin(9600);// 9600 is the speed of the connection.
 }
 
 // variable to specify which LED to light up
@@ -16,12 +12,8 @@ void loop() {
   // light up the specified LED
   SpinWheel.setLargeLED(which_LED, 255, 0, 0); 
   SpinWheel.drawFrame(); 
-  // Send a message to the connected computer.
-  // The message will be which LED is lit up.
-  Serial.println(which_LED);
   // add in a delay to make the LEDs change at a reasonable speed
   delay(500);
-  // Increase the value of the which_LED by 1.
+  // Increase the value of which_LED by 1.
   which_LED = which_LED + 1; 
-
 }
