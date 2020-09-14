a171d01280a39c88cd79b58bb1473eb7e726ff12
Stefan Krastanov <stefan@krastanov.org>
2020-09-14 03:38:41 -0400

    Fix inconsistent whitespaces and comment capitalization in Which_LED.

--- examples/Paper_Guide/Which_LED/Which_LED.ino
+++ examples/Paper_Guide/Which_LED/Which_LED.ino
@@ -1,14 +1,15 @@
 #include "SpinWearables.h"
 using namespace SpinWearables;
+
 void setup() {
   SpinWheel.begin();
 }
 
 void loop() {
-  // create a variable called which_LED
+  // Create a variable called which_LED.
   int which_LED = 1; 
-  // light up the Large LED that corresponds
-  // to which_LED
+  // Light up the Large LED that corresponds
+  // to which_LED.
   SpinWheel.setLargeLED(which_LED, 255, 0, 0); 
   SpinWheel.drawFrame(); 
 }
