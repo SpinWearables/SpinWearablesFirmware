038b9c6d7813c99e4a8c722aaa37ce23c8c3f352
Stefan Krastanov <stefan@krastanov.org>
2020-09-14 03:27:16 -0400

    Fix compilation error typos in Purple_LEDs.

--- examples/Paper_Guide/Purple_LEDs/Purple_LEDs.ino
+++ examples/Paper_Guide/Purple_LEDs/Purple_LEDs.ino
@@ -2,12 +2,10 @@
 using namespace SpinWearables;
 
 void setup() {
-  // put your setup code here, to run once:
-  SpinWheel.begin()
+  SpinWheel.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   SpinWheel.setLargeLEDsUniform(255, 0, 255);
   SpinWheel.drawFrame();
 }
